595248733e8fe79477b8190ecc501087d9a04bb3
JRCorp10 <ju4niru1z@gmail.com>
2022-08-08 20:57:25 -0300

    Update.

--- 3. Juani/Template.py
+++ 3. Juani/Template.py
@@ -1,3 +1,4 @@
+# Pregunta si quiero entrar al programa.
 pregunta = input("Quiere ingresar al programa? (1=Si,2=No): ")
 pregunta = int(pregunta)
 
@@ -5,6 +6,6 @@ while pregunta == 1:
 
     # Contenido del código
 
-
+    # Pregunta si deseo continuar.
     pregunta = input("Desea Continuar? (1=Si,2=No): ")
     pregunta = int(pregunta)
