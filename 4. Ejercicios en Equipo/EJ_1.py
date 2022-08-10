f1bc472056e9a6a6742f1e92765690ac74bc9937
JRCorp10 <ju4niru1z@gmail.com>
2022-08-10 19:32:20 -0300

    Updated.

--- 4. Ejercicios en Equipo/EJ_1.py
+++ 4. Ejercicios en Equipo/EJ_1.py
@@ -18,3 +18,20 @@ Crear un programa que pida por terminal:
         - Cuanto debe pagar (Teniendo en cuenta que los precios están sin IVA, debemos sumarle el IVA)
         - Al final del programa debe decir "Gracias por su compra y el nombre del comprador".
 """
+
+
+"""
+    Modifique el ejercicio anterior para que la persona pueda ingresar hasta un máximo de 10 facturas.
+    Usar al menos 4 funciones, cada una de ellas tiene que tener un proposito.
+    Todas las funciones deben estar en un archivo separado y ser llamadas desde el archivo main del código.
+    Una función debe representar el stock de los 3 items (Todos inicializados en 1000):
+        - Cada vez que se carga una factura debe restar la cantidad comprada.
+        - En caso de que el stock sea 0, debe informar al final, un mensaje que diga "Sin stock, su producto llegará en el transcuro de las 48hs a su domicilio.".
+    Una vez finalizada la carga de facturas:
+        - Mostrar la/s factura/s cargadas por NOMBRE y un ID único.
+        - Dar la opción de mostrar dichas las facturas hasta que la persona desee finalizar.
+    Mostrar:
+        - Todo lo mismo que en el ejercicio anterior.
+        - En caso de pagar en efectivo se aplicara un descuento del 10%.
+        - Mostrar stock remanente o mensaje depende el caso.
+"""
