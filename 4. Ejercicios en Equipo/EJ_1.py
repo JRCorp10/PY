766355956483b38d856d9ab3bdc49c401e03e2c7
JRCorp10 <ju4niru1z@gmail.com>
2022-08-09 21:02:30 -0300

    Primer EJ

---
+++ 4. Ejercicios en Equipo/EJ_1.py
@@ -0,0 +1,20 @@
+"""
+Crear un programa que pida por terminal:
+    - Nombre
+    - Telefono
+    - Dirección
+    - DNI
+    - FECHA DE VENTA
+    - PUNTO DE VENTA (Salon o Tienda Online)
+    - ARTICULO (1,2,3)
+    - CANTIDAD
+    - PRECIO UNITARIO
+    - EFECTIVO o TARJETA
+
+    Preguntar si desea iniciar el programa y preguntar si desea volver a cargar la factura o finalizar.
+    Mostrar:
+        - Ticket con datos de la compra.
+        - Si paga en efectivo no se le agrega el IVA, si paga con tarjeta se le suma el IVA y un 40%.
+        - Cuanto debe pagar (Teniendo en cuenta que los precios están sin IVA, debemos sumarle el IVA)
+        - Al final del programa debe decir "Gracias por su compra y el nombre del comprador".
+"""
