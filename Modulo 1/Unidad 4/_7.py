43217f8279b6de09cabd156c20fbe593037491f7
JRCorp10 <ju4niru1z@gmail.com>
2022-09-07 22:49:21 -0300

    Commit

--- Modulo 1/Unidad 4/_7.py
+++ Modulo 1/Unidad 4/_7.py
@@ -1,7 +1,7 @@
 lista = [
-    ["Elemento1","Elemento2","Elemento3"],
-    ["Elemento1","Elemento2","Elemento3"],
-    ["Elemento1","Elemento2","Elemento3"]
+    ["elemento1n1","elemento2n1","elemento3n1"],
+    ["elemento1n2","elemento2n2","elemento3n2"],
+    ["elemento1n3","elemento2n3","elemento3n3"]
 ]
 
 def recorre_lista(item, index):
